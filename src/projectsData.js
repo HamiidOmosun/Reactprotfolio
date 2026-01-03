31614391d27419a475feccb5f4a2ee3dc317264c
HamiidOmosun <Abdulhamiidomosun@gmail.com>
2026-01-03 16:09:58 +0100

    added proper link to projects

--- src/projectsData.js
+++ src/projectsData.js
@@ -12,22 +12,23 @@ const projectsData = [
     title: "sira admin dashboard",
     image: "/src/assets/Dashboard.png",
     description: "Sira dashbaord is a client project created to be the super admin for a mobile appilcation that exchanges giftcards",
-    githubLink: "https://github.com/HamiidOmosun/sira_admin"
+    liveLink: "https://github.com/HamiidOmosun/admin-panel-for-sira-mobile-app",
+    githubLink: "https://github.com/HamiidOmosun/admin-panel-for-sira-mobile-app"
   },
   {
     id: 3,
+    title: "Movie App",
+    image: "/src/assets/movieapi.png",
+    description: "An api based web application used to check and look up trending movies, this projects was done to explore the use of api and mapping data to interface",
+    liveLink : "https://movie-app-api-ruby.vercel.app/",
+    githubLink: "https://github.com/HamiidOmosun/movie-app-api"
+  },
+  {
+    id: 4,
     title: "Crypto wallet",
     image: "/src/assets/CryptoDark.png",
     description: "Crypto wallet in progress created to showcase design skills and tech stack from projects to projects",
     liveLink: "unavailable",
-    githubLink: "unavailable"
-  },
-  {
-    id: 4,
-    title: "Movie App",
-    image: "/src/assets/movieapi.png",
-    description: "An api based web application used to check and look up trending movies, this projects was done to explore the use of api and mapping data to interface",
-    liveLink : "unavailable",
     githubLink: "unavailable"
   }
 ]
