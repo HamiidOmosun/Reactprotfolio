6c5e61c7e7cdff9292757f93e5232ddf0ff07413
HamiidOmosun <Abdulhamiidomosun@gmail.com>
2026-01-05 09:33:00 +0100

    fixed import issues in peoject data

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,8 +1,15 @@
+import boscotti from "../src/assets/boscottiImage.png";
+import dashbaord from "../src/assets/Dashboard.png";
+import movieapi from "../src/assets/movieapi.png";
+import CryptoDark from "../src/assets/CryptoDark.png"
+
+
+
 const projectsData = [
   {
     id: 1,
     title: "Boscotti",
-    image: "/src/assets/boscottiImage.png",
+    image: boscotti,
     description: "Ecommerce store for a perfume reseller, built with React.",
     liveLink: "https://boscotti.vercel.app/",
     githubLink: "https://github.com/HamiidOmosun/boscotti"
@@ -10,7 +17,7 @@ const projectsData = [
   {
     id: 2,
     title: "sira admin dashboard",
-    image: "/src/assets/Dashboard.png",
+    image: dashbaord,
     description: "Sira dashbaord is a client project created to be the super admin for a mobile appilcation that exchanges giftcards",
     liveLink: "https://github.com/HamiidOmosun/admin-panel-for-sira-mobile-app",
     githubLink: "https://github.com/HamiidOmosun/admin-panel-for-sira-mobile-app"
@@ -18,7 +25,7 @@ const projectsData = [
   {
     id: 3,
     title: "Movie App",
-    image: "/src/assets/movieapi.png",
+    image: movieapi,
     description: "An api based web application used to check and look up trending movies, this projects was done to explore the use of api and mapping data to interface",
     liveLink : "https://movie-app-api-ruby.vercel.app/",
     githubLink: "https://github.com/HamiidOmosun/movie-app-api"
@@ -26,7 +33,7 @@ const projectsData = [
   {
     id: 4,
     title: "Crypto wallet",
-    image: "/src/assets/CryptoDark.png",
+    image: CryptoDark,
     description: "Crypto wallet in progress created to showcase design skills and tech stack from projects to projects",
     liveLink: "unavailable",
     githubLink: "unavailable"
